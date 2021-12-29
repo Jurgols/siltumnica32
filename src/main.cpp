571994461364a6bac91bf27a598ef85a0667b110
Jurgols <jurgols@gmail.com>
2021-12-29 11:24:48 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,327 @@
+#include <Arduino.h>
+#include <WiFiMulti.h>
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+#include <AM232X.h>
+#include <ESPmDNS.h>
+#include <AM232X.h>
+#include "Wire.h"
+#include "time.h"
+
+
+WiFiMulti wifiMulti;
+AM232X AM2320;
+
+
+#define lightPin 19
+#define heatPin 18
+#define fanOutPin 5
+#define fainInPin 17
+#define soilPin 34
+#define levelPin 35
+#define capPin 32
+#define mistPin 16
+#define waterPin 4
+#define lightInterupt 15
+
+// Volumetric water content calculation parameters
+float slope = 2.48; // slope from linear fit
+float intercept = -0.72;
+
+//InfuxDB device name
+#define DEVICE "Siltumica_ESP32"
+// InfluxDB  server url. Don't use localhost, always server name or ip address.
+#define INFLUXDB_URL "https://europe-west1-1.gcp.cloud2.influxdata.com"
+// InfluxDB 2 server or cloud API authentication token
+#define INFLUXDB_TOKEN ***REMOVED***
+// InfluxDB 2 organization id
+#define INFLUXDB_ORG ***REMOVED***
+// InfluxDB 2 bucket name
+#define INFLUXDB_BUCKET "siltumnica"
+//Riga time
+#define TZ_INFO "EET-2EEST,M3.5.0/3,M10.5.0/4"
+// InfluxDB client instance
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+// InfluxDB client instance for InfluxDB 1
+//InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+// Data point
+Point sensor("microclimate");
+char hostString[16] = {0};
+
+const char* mqtt_server = "192.168.88.192";
+//char mqtt_server[];
+//mqtt message buffer size
+#define MSG_BUFFER_SIZE  (50)
+unsigned long lastMsg = 0;
+char msg[MSG_BUFFER_SIZE];
+//init WiFi
+WiFiClient espClient;
+// init Mqtt client
+PubSubClient mqtt_client(espClient);
+//Deconstruct ctime struct
+struct tm * timeinfo;
+
+// Soil sensor mesure voltage
+float soil_voltage(uint8_t pin){
+  int soil_adc = analogRead(pin);
+  float voltage = (soil_adc / 4095.0) * 3.3;
+  return voltage;
+}
+// vwc calculate precentage from voltage
+float soil_vwc(float voltage){
+  float soil_percent = constrain((((1.0 / voltage) * slope) + intercept) * 100, 0, 100);
+  return soil_percent;
+}
+
+void reconnect();
+void callback(char*, byte*, unsigned int);
+
+
+class InfluxData
+{
+  long interval;
+  unsigned long previousMillis;
+  public:
+  InfluxData(int min_interval){
+    previousMillis = 0;
+    interval = min_interval * 60000; //convert minutes to milis
+  }
+  void Upload()
+  {
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis >= interval)
+    {
+      AM2320.begin();
+      digitalWrite(capPin, HIGH);
+      timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+      sensor.clearFields();
+      // Report RSSI of currently connected network
+      sensor.addField("rssi", WiFi.RSSI());
+      // Print what are we exactly writing
+      Serial.print("Writing: ");
+      Serial.println(client.pointToLineProtocol(sensor));
+      sensor.addField("rssi", WiFi.RSSI());
+      sensor.addField("air_temperature", AM2320.getTemperature());
+      sensor.addField("air_humidity", AM2320.getHumidity());
+      sensor.addField("soil_vwc", soil_vwc(soil_voltage(soilPin)));
+      sensor.addField("soil_moisture_voltage", soil_voltage(soilPin));
+      digitalWrite(capPin, LOW);
+      // Write point
+      if (!client.writePoint(sensor)) {
+        Serial.print("InfluxDB write failed: ");
+        Serial.println(client.getLastErrorMessage());
+      }
+      previousMillis = currentMillis;
+    }
+  }
+
+};
+class Lights
+{
+  int end_hour;
+  char light_pin;
+  int start_hour;
+  public:
+  Lights(char out_pin, int start_h, int end_h)
+  {
+    light_pin = out_pin;
+    start_hour = start_h;
+    end_hour = end_h;
+  }
+  void Update()
+  {
+    
+    if(start_hour >= end_hour)
+    {
+      if((timeinfo->tm_hour >= start_hour) || (timeinfo->tm_hour < end_hour))
+      {
+        digitalWrite(light_pin, HIGH);
+      }
+      else
+      {
+        //Turn on light when RPi is taking picture
+        if (digitalRead(lightInterupt))
+        {
+          digitalWrite(lightPin, HIGH);
+        }
+        else
+        {
+          digitalWrite(lightPin, LOW);
+        }
+    //Turn on at predefined interval
+      }
+    }
+    else
+    {
+      if((timeinfo->tm_hour >=start_hour) && (timeinfo->tm_hour < end_hour))
+      {
+        digitalWrite(light_pin, HIGH);
+      }
+      else
+      {
+        //Turn on light when RPi is taking picture
+        if (digitalRead(lightInterupt))
+        {
+          digitalWrite(lightPin, HIGH);
+        }
+        else
+        {
+          digitalWrite(lightPin, LOW);
+        }
+      }
+
+    }
+  }
+};
+
+Lights growLights(lightPin, 7, 1);
+InfluxData sensorData(5);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  pinMode(lightPin, OUTPUT);
+  pinMode(heatPin, OUTPUT);
+  pinMode(fanOutPin, OUTPUT);
+  pinMode(fainInPin, OUTPUT);
+  pinMode(soilPin, INPUT);
+  pinMode(mistPin, OUTPUT);
+  pinMode(waterPin, OUTPUT);
+  pinMode(lightInterupt, INPUT);
+  pinMode(levelPin, INPUT);
+  pinMode(capPin, OUTPUT);
+  digitalWrite(capPin, LOW);
+  digitalWrite(lightPin, LOW);
+  digitalWrite(heatPin, LOW);
+  digitalWrite(fanOutPin, LOW);
+  digitalWrite(fainInPin, HIGH);
+  digitalWrite(mistPin, LOW);
+  digitalWrite(waterPin, LOW);
+  //setup mDNS responder id 
+
+  // Connect WiFi
+  Serial.println("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(***REMOVED***, ***REMOVED***);
+  wifiMulti.addAP(***REMOVED***, ***REMOVED***);
+  wifiMulti.addAP(***REMOVED***, ***REMOVED***);
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  ArduinoOTA.setHostname("siltumnica");
+  ArduinoOTA.setPassword("esp8266");
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+
+  if (!MDNS.begin(hostString)) {
+    Serial.println("Error setting up MDNS responder!");
+  }
+  Serial.println("mDNS responder started");
+
+  Serial.println("Sending mDNS query");
+  int n = MDNS.queryService("mqtt", "tcp"); 
+  Serial.println("mDNS query done");
+  if (n == 0) {
+    Serial.println("no services found");
+  }
+  else {
+    Serial.print(n);
+    Serial.println(" service(s) found");
+    Serial.print(MDNS.IP(0));
+    //not working
+    //mqtt_server = toCharArray(MDNS.IP(0)[0]);
+  }
+
+  //Sync time with ntp
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+  // Add constant tags - only once
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  mqtt_client.setServer(mqtt_server, 1883);
+  mqtt_client.setCallback(callback);
+}
+
+unsigned long long recconectMillis = 0;
+int recconectInterval = 600000;
+
+void loop() {
+  unsigned long long currentMillis = millis();
+
+  ArduinoOTA.handle();
+  if ((!mqtt_client.connected()) && (currentMillis - recconectInterval >= recconectInterval)) 
+  {
+    reconnect();
+  }
+  mqtt_client.loop();
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+  time_t now;
+  time(&now);
+  timeinfo = localtime(&now);
+  growLights.Update();
+  sensorData.Upload();
+
+  
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  if (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (mqtt_client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+    }
+  }
+}
